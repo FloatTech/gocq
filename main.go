cff744e3cc2639807f8f18f7d3b4cbd7561f654e
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-03-19 12:48:26 +0800

    change db to sqlite3

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"github.com/Mrs4s/go-cqhttp/cmd/gocq"
 	"github.com/Mrs4s/go-cqhttp/global/terminal"
 
-	_ "github.com/Mrs4s/go-cqhttp/db/leveldb"   // leveldb 数据库支持
+	//_ "github.com/Mrs4s/go-cqhttp/db/leveldb"   // leveldb 数据库支持
 	_ "github.com/Mrs4s/go-cqhttp/modules/silk" // silk编码模块
 	// 其他模块
-	// _ "github.com/Mrs4s/go-cqhttp/db/sqlite3"   // sqlite3 数据库支持
+	_ "github.com/Mrs4s/go-cqhttp/db/sqlite3" // sqlite3 数据库支持
 	// _ "github.com/Mrs4s/go-cqhttp/db/mongodb"    // mongodb 数据库支持
 	// _ "github.com/Mrs4s/go-cqhttp/modules/pprof" // pprof 性能分析
 )
